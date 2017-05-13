2a507e4d9156b9e1810d6a9a0bc42f99fe881015
Abhishek Shende <abhishek.shende@ellimae.com>
2017-05-12 23:59:00 -0700

    add prettier and npm-sort

--- app/main.js
+++ app/main.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import App from './components/App';
+
 import rootReducer from './reducers';
 
 let store = createStore(
